e79df6f4dbdee6ba5803d2ea87d5e6d6147b102c
Afoghandi <ralphaeloshun@gmail.com>
2025-01-08 15:35:58 +0000

    animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,33 @@
 module.exports = {
+    mode: "jit",
     content: ["./Views/**/*.cshtml", "./wwwroot/**/*.js"],
     theme: {
         extend: {
             animation: {
                 fadeIn: "fadeIn 1s ease-in-out",
+                bounceIcon: "bounceIcon 2s infinite",
             },
             keyframes: {
                 fadeIn: {
                     "0%": { opacity: 0 },
                     "100%": { opacity: 1 },
                 },
-            },
-            colors: {
-                primary: "#4F7886", 
-                secondary: "#D1E8E4", 
-            },
-            fontFamily: {
-                sans: ['"Roboto"', "sans-serif"],
-            },
-        },
+                bounceIcon: {
+                    "0%, 100%": { transform: "translateY(0)" },
+                    "50%": { transform: "translateY(-10px)" },
+                },
 
+                colors: {
+                    primary: "#4F7886",
+                    secondary: "#D1E8E4",
+                },
+                fontFamily: {
+                    sans: ['"Roboto"', "sans-serif"],
+                },
+            },
+
+        },
     },
+    mode: "jit",
     plugins: [],
 };
